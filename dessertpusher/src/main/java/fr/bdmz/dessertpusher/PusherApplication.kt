504bf69e7ba0989d50939876a5967cd7715efe6a
M. Boudamouz <mboudamouz@reseau-delta.ch>
2020-03-18 17:57:59 +0100

    complete course #4

---
+++ dessertpusher/src/main/java/fr/bdmz/dessertpusher/PusherApplication.kt
@@ -0,0 +1,13 @@
+package fr.bdmz.dessertpusher
+
+import android.app.Application
+import timber.log.Timber
+
+class PusherApplication : Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+
+}
